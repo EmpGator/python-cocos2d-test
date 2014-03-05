0a6c5e91f8686d29a5451b16e639dbfa0be0a58f
henkka95 <henri.s.korhonen@gmail.com>
2014-03-05 22:18:38 +0200

    Initial commit

--- main.py
+++ main.py
@@ -28,15 +28,17 @@ class World(cocos.layer.Layer):
     def __init__(self):
         super(World, self).__init__()
         self.player = Car(self)
-        self.newMap()
+        self.newMap(30, 30)
         self.map = tiles.load('tilemap.xml')['map0']
         self.blockKeys = 0.0
-        self.blockKeysFull = 0.5
+        self.blockKeysFull = 0.21
         self.bindings = {
             key.LEFT: 'left',
             key.RIGHT: 'right',
             key.UP: 'up',
             key.DOWN: 'down',
+            key.W: 'up2',
+            key.Z: 'scale',
                         }
         buttons = {}
         for k in self.bindings:
@@ -52,8 +54,8 @@ class World(cocos.layer.Layer):
         self.schedule(self.update)
 
 
-    def newMap(self):
-        xmlmap_maker.newMap(60, 60)
+    def newMap(self, x=30, y=30):
+        xmlmap_maker.newMap()
 
     def on_key_press(self, k, m):
         binds = self.bindings
@@ -74,11 +76,19 @@ class World(cocos.layer.Layer):
 
         if self.blockKeys > 0:
             self.blockKeys -= dt
+        if self.blockKeys <= 0:
+            cell = self.map.get_at_pixel(self.player.x, self.player.y)
+            x2, y2 = cell.x, cell.y
+            self.player.x = x2+self.player.movAmount/2
+            self.player.y = y2+self.player.movAmount/2
 
         buttons = self.buttons
         if buttons['up'] and self.blockKeys <= 0:
             self.blockKeys = self.blockKeysFull
             self.player.forward()
+        elif buttons['up2'] and self.blockKeys <= 0:
+            self.blockKeys = self.blockKeysFull
+            self.player.forward(False)
         elif buttons['left'] and self.blockKeys <= 0:
             self.blockKeys = self.blockKeysFull
             self.player.turnLeft()
@@ -88,6 +98,11 @@ class World(cocos.layer.Layer):
         elif buttons['down'] and self.blockKeys <= 0:
             self.blockKeys = self.blockKeysFull
             self.player.turnBack()
+        elif buttons['scale'] and self.blockKeys <= 0:
+            if self.manager.scale == .25:
+                self.manager.do(actions.ScaleTo(1, 2))
+            else:
+                self.manager.do(actions.ScaleTo(.25, 2))
 
 
 class Car(cocos.sprite.Sprite):
@@ -102,8 +117,8 @@ class Car(cocos.sprite.Sprite):
         self.owner = owner
         self.x = 64*7
         self.y = 64*7
-        self.animTime = 0.5
-        self.rotTime = 0.4
+        self.animTime = 0.2
+        self.rotTime = 0.2
         self.orientation = UP
         self.movAmount = 128
 
@@ -130,14 +145,18 @@ class Car(cocos.sprite.Sprite):
     def turnBack(self):
         self.do(actions.RotateBy(-180, self.rotTime))
 
-    def forward(self):
-        #TODO Make sure that player stays in middle of cell
+    def forward(self,check=True):
         cell = self.owner.map.get_at_pixel(self.x, self.y)
         self.checkOrientation()
         x, y = self.orientation
         cell = self.owner.map.get_neighbor(cell, self.orientation)
-        if not "wall" in cell.tile.id:
-            self.do(actions.MoveBy((x*self.movAmount, y*self.movAmount), self.animTime))
+
+        try:
+            if not "wall" in cell.tile.id or not check:
+                self.do(actions.MoveBy((x*self.movAmount, y*self.movAmount), self.animTime))
+        except AttributeError:
+            print "You are trying to exceed map limits"
+
 
 
 def main():
--- xmlmap_maker.py
+++ xmlmap_maker.py
@@ -1,13 +1,168 @@
 # -*- coding:utf-8 -*-
 __author__ = 'henri'
 
+from random import choice, randint
+import AStar
 
-def newMap(x, y, tilesetfile="road-tiles.xml"):
+def newRoom(minX,maxX,minY,maxY):
+    roomWidth = randint(minX, maxX)+2
+    roomHeight = randint(minY, maxY)+2
+    return [roomWidth, roomHeight]
+
+def dungeonGenerator():
+    width = 60
+    height = 60
+    roomsMin = 22
+    roomsMax = 24
+    minRoomWidth = 2
+    maxRoomWidth = 6
+    minRoomHeight = 2
+    maxRoomHeight = 6
+    generatedRooms = 0
+    dungeon = []
+    rooms = []
+
+    roomAmount = randint(roomsMin, roomsMax)
+
+    #Adds rooms to rooms list
+    for x in range(roomAmount):
+        rooms.append(newRoom(minRoomWidth, maxRoomWidth, minRoomHeight, maxRoomHeight))
+
+
+    #Makes map with walls
+    for y in range(height):
+        container = []
+        for x in range(width):
+            if x > 2 and x < width-3 and y > 2 and y < height-3:
+                container.append(".")
+            else:
+                container.append("#")
+        dungeon.append(container)
+
+    #Check wheter room should be created
+    for rowId in range(len(dungeon)):
+        for charId in range(len(dungeon[rowId])):
+            if dungeon[rowId][charId] != '#':
+                startGeneratingRoomHere = randint(1, 10)
+                if startGeneratingRoomHere == 1:
+                    generatedRooms += CreateRoom(dungeon, charId, rowId, rooms) #Tries to make room
+
+    #Prints map
+    for rowId in range(len(dungeon)):
+        for charId in range(len(dungeon[rowId])):
+            if dungeon[rowId][charId] == 'D':
+                createPaths(rowId, charId, dungeon)
+
+    for row in dungeon:
+        container = ''
+        for char in row:
+            container += char
+        print container
+
+    print "Generated rooms", str(generatedRooms)
+    return dungeon
+
+
+def CreateRoom(dungeon, x, y, rooms):
+    """
+
+    :rtype : int
+    """
+    if rooms != []:
+        room = rooms[0]
+    else:
+        return 0
+
+    maxX = x+room[0]
+    maxY = y+room[1]
+    door = 0
+
+    #Make sure room isn't generated over wall
+    if maxX > len(dungeon[0])-3 or maxY > len(dungeon)-3:
+        rooms.append(rooms[0])
+        rooms.pop(0)
+        return 0
+
+    #Check if there are any rooms too close
+    for yId in range(y-1, maxY+1):
+        for xId in range(x-1, maxX+1):
+            if 'R' == dungeon[yId][xId] or 'F' == dungeon[yId][xId]:
+                rooms.append(rooms[0])
+                rooms.pop(0)
+                return 0
+
+    #Create rooms for dungeon
+    for yId in range(y, maxY):
+        for xId in range(x, maxX):
+            dungeon[yId][xId] = 'R'
+            if xId > x and xId < maxX-1 and yId > y and yId < maxY-1:
+                dungeon[yId][xId] = 'F'
+            else: #Create walls and door
+                if (xId != x and xId != maxX-1) or (yId != y and yId != maxY-1):
+                    if dungeon[yId-1][xId] != '#' and dungeon[yId+1][xId] != '#':
+                        if dungeon[yId][xId+1] != '#' and dungeon[yId][xId-1] != '#':
+                            createDoor = randint(1, 6)
+                            if yId == maxY-1 and xId == maxX-2:
+                                createDoor = 1
+                            if createDoor == 1 and door == 0:
+                                dungeon[yId][xId] = 'D'
+                                door = 1
+                            else:
+                                dungeon[yId][xId] = 'R'
+                else:
+                    dungeon[yId][xId] = 'R'
+
+    rooms.pop(0)
+    return 1
+
+
+def createPaths(y, x, dungeon):
+    """
+    Create paths
+    from door to door or
+    from path to door or
+    from door to path or
+    from door to dead end or
+    from path to dead end
+    """
+    mapDataForAstar = []
+    doors = []
+
+    #dungeon[y][x] = 'S'
+    for rowId in range(len(dungeon)):
+        for charId in range(len(dungeon[rowId])):
+            if dungeon[rowId][charId] in "#RF":
+                mapDataForAstar.append(-1)
+            elif dungeon[rowId][charId] == "D":
+                if x != charId and y != rowId:
+                    doors.append([rowId, charId])
+                mapDataForAstar.append(1)
+            else:
+                mapDataForAstar.append(1)
+
+    astar = AStar.AStar(AStar.SQ_MapHandler(mapDataForAstar, len(dungeon[0]), len(dungeon)))
+    start = AStar.SQ_Location(x, y)
+    end = AStar.SQ_Location(doors[0][0], doors[0][1])
+
+    p = astar.findPath(start, end)
+
+    if not p:
+        print "No path found!"
+        pathlines = []
+    else:
+        print "Found path in",str(len(p.nodes)),"moves."
+        pathlines = []
+        #pathlines.append((start.x, start.y))
+        for n in p.nodes:
+            pathlines.append((n.location.x, n.location.y))
+        pathlines.append((end.x, end.y))
+
+    for point in pathlines:
+        dungeon[point[1]][point[0]] = "P"
+
+def newMap(tilesetfile="road-tiles.xml"):
     import xml.etree.ElementTree as ET
-    from random import choice
 
-    columns = y
-    rows = x
     idList = []
 
     try:
@@ -27,11 +182,29 @@ def newMap(x, y, tilesetfile="road-tiles.xml"):
     rE = ET.Element('resource')
     rqE = ET.SubElement(rE, 'requires', {'file': tilesetfile})
     reE = ET.SubElement(rE, 'rectmap', {'id': "map0", 'origin': "0,0,0", 'tile_size': "128x128"})
+
+    #print idList
+    dungeon = dungeonGenerator()
+
+    idList = {
+        '#': 'blackwall',
+        '.': 'concrete_wall',
+        'R': 'orange_wall',
+        'F': 'lighthumanskin',
+        'P': 'greenish-yellow',
+        'D': 'humanskin'
+    }
+
+    rows = len(dungeon)
+    columns = len(dungeon[0])
+
     for x in range(rows):
         column = ET.SubElement(reE, 'column')
         for y in range(columns):
-            randId = choice(idList)
-            ET.SubElement(column, 'cell', {'tile': randId})
+            key = dungeon[y][x]
+            tileId = idList[key]
+            #randId = choice(idList)
+            ET.SubElement(column, 'cell', {'tile': tileId})
 
     xmlContent = ET.tostring(rE)
     xmlFile = open('tilemap.xml','w+')
@@ -39,6 +212,7 @@ def newMap(x, y, tilesetfile="road-tiles.xml"):
     xmlFile.close()
 
 if __name__ == '__main__':
-    x = input("Rows?: ")
-    y = input("Columns? ")
-    newMap(x,y)
+    #x = input("Rows?: ")
+    #y = input("Columns? ")
+    newMap(3, 3)
+    #dungeonGenerator()
